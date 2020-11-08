e3506f390f4887b3c24266cc3aef4dbc38b57b02
Павел <p-trishkin1@mail.ru>
2020-11-08 06:43:04 +0300

    fix

--- src/main/java/ru/gb/trishkin/shop/config/SellIntegrationConfig.java
+++ src/main/java/ru/gb/trishkin/shop/config/SellIntegrationConfig.java
@@ -1,6 +1,6 @@
 package ru.gb.trishkin.shop.config;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.integration.channel.DirectChannel;
@@ -10,7 +10,7 @@ import org.springframework.integration.channel.DirectChannel;
 public class SellIntegrationConfig {
     private DirectChannel sellsChannel;
 
-    public SellIntegrationConfig(@Qualifier(value = "sellsChannel") DirectChannel sellsChannel) {
+    public SellIntegrationConfig(DirectChannel sellsChannel) {
         this.sellsChannel = sellsChannel;
     }
 
